2ddb411059f88227b6ce36abbc39ab5c400a32c3
Joshua Kiya <69136659+JoshKiyakoo@users.noreply.github.com>
2022-04-19 17:34:11 +0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,30 @@
+<?php
+// ======================================================================
+//             Designed by:    Eyasu Girma [Kiya]
+//                 Mobile:     +251-913-078-029
+//                 Email:      sendtokiya@gmail.com
+//                 Facebook:   https://facebook.com/JoshKiyakoo
+//                 LinkedIn:   https://linkedin.com/in/JoshKiyakoo
+//                 Twitter:    https://twitter.com/JoshKiyakoo
+//                 GitHub:     https://github.com/JoshKiyakoo
+//                 Telegram:   https://t.me/JoshKiyakoo
+// ======================================================================
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
